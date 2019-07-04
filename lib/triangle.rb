7548fc578cd67e891c6378401d28a1e7a2c56986
Marc Poppy <poppy.marc@gmail.com>
2019-07-04 02:56:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,8 +3,8 @@ class Triangle
   attr_accessor :length_one, :length_two, :length_three
 
   def initialize(length_one:, length_two:, length_three:)
-    @equilateral = equilateral
-    @isosceles = isosceles
-    @scalene = scalene
+    @length_one = length_one
+    @length_two = length_two
+    @length_three = length_three
   end
 end
